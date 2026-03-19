00f34060d8e3b2b2c07102b5723f964e4a2f2c95
Nullora <nullora@tutamail.com>
2026-03-19 08:52:11 +0100

    double buffer and welcomes

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -8,15 +8,10 @@ void main(BootInfo* bootinfo) {
     void* backbuffer = malloc(bootinfo->FrameBufferSize);
     clear_screen(0x000000, bootinfo, backbuffer);
 
-    draw_hex(heap_base, 50, 50, 0xFFFFFF, bootinfo,backbuffer);
-    //screenbuffer and backbuffer
-    draw_hex((unsigned long long)screenbuffer, 50, 70, 0xFFFFFF, bootinfo,backbuffer);
-    draw_hex((unsigned long long)backbuffer, 50, 90, 0xFFFFFF, bootinfo, backbuffer);
-    //framebuffer size and descriptor size
-    draw_hex((unsigned long long)bootinfo->FrameBufferSize, 50, 110, 0xFFFFFF, bootinfo, backbuffer);
-    draw_hex((unsigned long long)bootinfo->DescriptorSize, 50, 130, 0xFFFFFF, bootinfo, backbuffer);
-    //memMap size
-    draw_hex((unsigned long long)bootinfo->mMapSize, 50, 150, 0xFFFFFF, bootinfo, backbuffer);
+    draw_string(">Printed by NovusKernel and loaded from NBL", 50, 50, 0xFFFFFF, bootinfo, backbuffer);
+    draw_string(">Made by @Nullora on GitHub", 50, 70, 0xFFFFFF, bootinfo, backbuffer);
+    draw_string(">Double buffering and heap setup", 50, 90, 0xFFFFFF, bootinfo, backbuffer);
+
     memcpy(screenbuffer, backbuffer, bootinfo->FrameBufferSize);
     for(;;);
 }
